1b7ec14461c993eab45b939d3750f1333faaf2c7
gadiaz4 <65507572+gadiaz4@users.noreply.github.com>
2020-06-12 21:08:35 -0500

    Create Ejercicio_16.java

---
+++ Ejercicio_16.java
@@ -0,0 +1,29 @@
+/*
+Implemente un algoritmo que le despleque la sintaxis del uso de constantes
+ */
+package ejercicio_16;
+/**
+ *
+ * @author Gabriel Alexander Díaz Vera
+ */
+import java.util.Scanner;
+public class Ejercicio_16 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Scanner objeto=new Scanner (System.in);
+    System.out.println("\t\t\tUNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE");
+    System.out.println("Nombre: Díaz Vera Gabriel Alexander");
+    System.out.println("Asignatura: Programación");
+    System.out.println("Nrc: 7450");
+    System.out.println("Tema del Paper:");
+    System.out.println("Transferencia de Aprendizaje Mediante Redes Neuronales Convolucionales para el Reconocimiento de Conductores Distraídos\n");
+    System.out.println("\t\t\tUSO CORRECTO DE IDENTIFICADORES\n");
+    System.out.println("Nomeclatura de constantes \n\n");
+    System.out.println("#define NUMERO_PAGINA  5\n");
+    System.out.println("#define TITULO_PAPER 30\n");
+    System.out.println("#define DESCRIPCION_CONDUCTOR 10\n");
+    System.out.println("#define ENTRENAMIENTO 10\n");
+    }
+}
